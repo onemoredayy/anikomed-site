c7821028b7ca8cceaf4d7acfe177177155f1aa0e
onemoredayy <larinaa32@gmail.com>
2026-09-07 07:09:49 +0300

    Updates

--- src/constants/socialLinks.ts
+++
@@ -1,6 +0,0 @@
-export const SOCIAL_HREFS = {
-  vk: 'https://vk.ru/club238911776',
-  whatsapp: 'https://wa.me/message/QQ3FPC3CVVM4J1?src=qr',
-  telegram: 'https://t.me/AnikoMed_bot',
-  instagram: 'https://www.instagram.com/anikomed_studia?utm_source=qr&igsh=bWg0bnBhbDZzMXg0',
-} as const;
